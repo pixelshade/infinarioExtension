b367019913419f751cc3a213df7f9fcce2c4e5d6
pixelshade <psuldo@gmail.com>
2016-04-08 17:33:04 +0200

    working jquery

--- popup.js
+++ popup.js
@@ -9,6 +9,9 @@ document.addEventListener('DOMContentLoaded', function() {
   var checkPageButton = document.getElementById('checkPage');
   checkPageButton.addEventListener('click', function(tab) {
     
+    chrome.tabs.executeScript(null, { file: "jquery.js" }, function() {
     chrome.tabs.executeScript(null, {file: "check_attributes.js"});
+    });
+    
   }, false);
 }, false);
