1933c9b9ed7be036bedb267961d3ae0c7056db8a
pixelshade <psuldo@gmail.com>
2016-04-09 19:29:59 +0200

    working prototype

--- check_attributes.js
+++ check_attributes.js
@@ -1,66 +1,82 @@
 // (function() {
-    
-    
+
+
 // var checkAttributes = function(){
 
 var empty_attributes = [];
 var danger_attributes = [];
-$('.property-group tbody tr').each(function(i, val){
-    
-    setTimeout(function(){
+
+$('.property-group tbody tr').each(function(i, val) {
+
+    setTimeout(function() {
         var chil = $(val).children();
         var kuk = $(val).children().eq(1).children().first().children().first().children().first();
         var attr_name = chil.first().text();
 
         kuk[0].click();
-        setTimeout(function(){
+        setTimeout(function() {
             var total = 0;
             var total24 = 0;
             var is_negative = false;
             var is_empty = false;
             var entries = $('.report-table tr').length;
             var only_one_entry = "";
-            $('.report-table tr').each(function(index, value){
-                if(index <2) return;
+            var res = "";
+            $('.report-table tr').each(function(index, value) {
+                if (index < 2) return;
                 var c = $(this).children();
-                var p = c.eq(0).text().trim();  
+                var p = c.eq(0).text().trim();
                 only_one_entry = p;
-                if(p === "") is_empty = true;
-                if(!is_negative) is_negative = p.indexOf('-') === 0;
+                if (p === "") is_empty = true;
+                if (!is_negative) is_negative = p.indexOf('-') === 0;
                 var t = parseInt(c.eq(1).text().trim());
                 var l24 = parseInt(c.eq(3).text().trim());
-                total+=t;
-                total24 += l24;                
+                total += t;
+                total24 += l24;
             });
-            if(entries === 0) {
-              console.log("Please check again:", attr_name);              
-              chil.first().css("background","red");
+            if (entries === 0) {
+                console.log("Please check again:", attr_name);
+                res +="Please check again:" + attr_name + "\n";
+                chil.first().css("background", "red");
             }
-            if(entries === 3) {
-              console.log("Only one entry("+ only_one_entry+"):", attr_name);
-              chil.first().css("background","red");
+            if (entries === 3) {
+                console.log("Only one entry(" + only_one_entry + "):", attr_name);
+                res +="Only one entry(" + only_one_entry + "):" + attr_name + "\n";
+                chil.first().css("background", "red");
             }
-            if(is_empty) {
-              console.log("Contains empty:", attr_name);
-              chil.first().css("background","red");
+            if (is_empty) {
+                console.log("Contains empty:", attr_name);
+                res +="Contains empty:" + attr_name + "\n";
+                chil.first().css("background", "red");
             }
-            if(is_negative) {
-              console.log("Negative:", attr_name);
-              chil.first().css("background","red");
+            if (is_negative) {
+                console.log("Negative:", attr_name);
+                res +="Negative:" + attr_name + "\n";
+                chil.first().css("background", "red");
             }
-            if(total === 0) {
-              console.log("total=0",attr_name);
-              chil.first().css("background","red");
+            if (total === 0) {
+                console.log("total=0", attr_name);
+                res +="total=0" + attr_name + "\n";
+                chil.first().css("background", "red");
             }
-            if(total24 === 0) {
-              console.log("total24=0:",attr_name);
-              chil.first().css("background","red");
+            if (total24 === 0) {
+                console.log("total24=0:", attr_name);
+                res +="total24=0:" + attr_name + "\n";
+                chil.first().css("background", "red");
             }
-            
-            $('.close')[0].click();    
+
+            chrome.runtime.sendMessage({ line: res }, function(response) {
+                
+            });
+
+            $('.close')[0].click();
         }, 2000);
-    }, (i)*3000);
+    }, (i) * 3000);
 
 });
+
+
+
+
 // };
 // })();
--- popup.js
+++ popup.js
@@ -1,17 +1,34 @@
 document.addEventListener('DOMContentLoaded', function() {
-  chrome.tabs.getSelected(null, function(tab) {
-    console.log(tab);
-    d = document;
-    var f = d.createElement('textarea');
-    d.body.appendChild(f); 
-  });
+    var d = document;
+    var f = document.getElementById("out");
+    f.style.visibility = "hidden";
 
-  var checkPageButton = document.getElementById('checkPage');
-  checkPageButton.addEventListener('click', function(tab) {
-    
-    chrome.tabs.executeScript(null, { file: "jquery.js" }, function() {
-    chrome.tabs.executeScript(null, {file: "check_attributes.js"});
+    chrome.tabs.getSelected(null, function(tab) {
+        console.log(tab);
     });
-    
-  }, false);
+
+    var checkPageButton = document.getElementById('checkPage');
+    checkPageButton.addEventListener('click', function(tab) {
+        chrome.tabs.executeScript(null, { file: "jquery.js" }, function() {
+            chrome.tabs.executeScript(null, { file: "check_attributes.js" }, function(result) {
+                console.log(result);
+               f.value = "----\n";
+            });
+        });
+
+    }, false);
+
+    chrome.runtime.onMessage.addListener(
+        function(request, sender, sendResponse) {            
+            console.log(sender.tab ?
+                "from a content script:" + sender.tab.url :
+                "from the extension");
+                f.style.visibility = "visible";                
+                f.value += request.line;                
+            //  if (request.greeting == "hello")
+                // sendResponse({ farewell: "goodbye" });
+        });
+
+
+
 }, false);
